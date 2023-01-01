1da9a3ee76078ea004c3b7e0f07c872c8573a89a
GautierWiann <wianni.gautier@gmail.com>
2023-01-01 21:27:44 +0100

    rien

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = "mongodb+srv://AgenceNuisible:capsule57@testdev.4903omx.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
