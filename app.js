8d21d34679b36886fcdb54ff273631430f989a43
GautierWiann <wianni.gautier@gmail.com>
2023-02-07 10:52:50 +0100

    email plus détaillé

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({ "Access-Control-Allow-Origin": "*" }))
+app.use(cors({ "Access-Control-Allow-Origin": "https://agence-nuisibles.com" }))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
